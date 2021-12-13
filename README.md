7ac9f11b480b7c35b91afa605cf980b7aff86bc5
attilakeller <paragliders@gmail.com>
2021-12-13 06:41:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-5jg47w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-5jg47w)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+import { of, from } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  name = 'Angular ' + VERSION.major;
+
+  ngOnInit() {
+    of(2, 4, 6, 9).subscribe(console.log);
+    from([20, 15, 10, 5]).subscribe(
+      (item) => console.log(`resulting item... $(item)`),
+      (err) => console.error(`error occured $(err)`),
+      () => console.log('complete')
+    );
+  }
+}
