ca54c6423a9954c4c1d957f677212f05b4419665
attilakeller <paragliders@gmail.com>
2021-12-13 07:08:01 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, VERSION } from '@angular/core';
 import { of, from } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -11,10 +12,28 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     of(2, 4, 6, 9).subscribe(console.log);
-    from([20, 15, 10, 5]).subscribe(
-      (item) => console.log(`resulting item... $(item)`),
-      (err) => console.error(`error occured $(err)`),
-      () => console.log('complete')
-    );
+
+    from([1, 2, 3, 5, 7])
+      .pipe(
+        map((item) => item * 2),
+        map((item) => item - 2),
+        map((item) => {
+          if (item == 0) {
+            throw new Error('detected');
+          }
+          return item;
+        })
+      )
+      .subscribe({
+        next: (v) => console.log(`resulting item ${v}`),
+        error: (e) => console.error(`error occured ${e}`),
+        complete: () => console.log('complete'),
+      });
+
+    // from([20, 15, 10, 5]).subscribe(
+    //   (item) => console.log(`resulting item... $(item)`),
+    //   (err) => console.error(`error occured $(err)`),
+    //   () => console.log('complete')
+    // );
   }
 }
